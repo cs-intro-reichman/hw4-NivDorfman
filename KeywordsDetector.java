c5c5954840689b5f441794de39fcaf960375ccb0
NivDorfman <nivdorf@gmail.com>
2024-11-27 13:33:35 +0200

    Update KeywordsDetector.java

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -21,6 +21,13 @@ public class KeywordsDetector {
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
-        // Replace this comment with your code
+       for (String sentence : sentences) {
+            for (String keyword : keywords) {
+                // Check if the sentence contains the keyword
+                if (sentence.toLowerCase().contains(keyword.toLowerCase())) {
+                    System.out.println(keyword);
+                }
+            }
+        }
     }
 }
