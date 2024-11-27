2a764a9cd222c4758f2b23ec481db9101b57e01e
NivDorfman <nivdorf@gmail.com>
2024-11-27 17:13:31 +0200

    Update Primes.java

--- Primes.java
+++ Primes.java
@@ -1,5 +1,28 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+    int len = Integer.parseInt(args[0]);
+        boolean [] arr =new boolean[len+1];
+        arr[0] = false;
+        arr[1]= false;
+        int p = 2;
+        for (int i =2; i < arr.length; i++) {
+            arr[i] = true;
+        }
+
+
+        while( p < Math.sqrt(len))
+        {
+            for (int i = p+1; i < arr.length; i++) {
+
+                if( i % p == 0)
+                arr[i] = false;
+            }
+            p++;
+        }
+       
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        
+        }
     }
 }
