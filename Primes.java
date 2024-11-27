76e2ad23c85501f9edbd9c7af7d799b6913e026e
NivDorfman <nivdorf@gmail.com>
2024-11-27 17:27:53 +0200

    Update Primes.java

--- Primes.java
+++ Primes.java
@@ -1,6 +1,6 @@
 public class Primes {
     public static void main(String[] args) {
-    int len = Integer.parseInt(args[0]);
+           int len = Integer.parseInt(args[0]);
         boolean [] arr =new boolean[len+1];
         arr[0] = false;
         arr[1]= false;
@@ -19,10 +19,17 @@ public class Primes {
             }
             p++;
         }
-       
+       System.out.println("Prime numbers up to " + len + ":");
+       int cnt = 0;
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
+            if(arr[i] == true)
+            {
+                System.out.println(i);
+                cnt++;
+
+            }
         
         }
+        System.err.println("There are " + cnt + " primes bewtween 2 and " + len +  " (" + (100*cnt)/len + "% are primes)");
     }
 }
